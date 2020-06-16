75d42537278fbff38f3df7c84b7746e7ad159691
Souu99 <62961562+Souu99@users.noreply.github.com>
2020-06-17 01:53:21 +0200

    Add files via upload

---
+++ Souheil/Tache 1_Enigme/Makefile
@@ -0,0 +1,6 @@
+prog:main.o enigme.o
+	gcc main.o enigme.o -o prog -lSDL_image -lSDL_ttf -lSDL_mixer -lSDL -g
+main.o:main.c
+	gcc -c main.c -g
+enigme.o:enigme.c
+	gcc -c enigme.c -g
---
+++ Souheil/Tache 1_Enigme/enigme.h
@@ -0,0 +1,48 @@
+/**
+* @file main.c
+* @brief Testing Program.
+* @author C Team
+* @version 0.1
+* @date Apr 01, 2015
+*
+* Testing program for background scrollilng
+*
+*/
+#ifndef ENIGME_H_INCLUDED
+#define ENIGME_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_ttf.h> 
+#include <SDL/SDL_mixer.h>
+
+typedef struct 
+{
+	SDL_Surface *background[2];
+	SDL_Surface *boutons[5];
+	SDL_Surface *reponses[5];
+	SDL_Surface *question;
+	SDL_Surface *win;
+	SDL_Surface *lost;
+	
+	SDL_Rect posRepones[5];
+	SDL_Rect posQuestion;
+	int choix_question;
+	int image_courrante;
+	int positionVraiReponse;
+          
+      
+}Enigme;
+
+
+void Init_Enigme(Enigme *e);
+int Play_Enigme(Enigme *e,SDL_Surface *ecran,int *Game);
+void Display_Enigme(Enigme *e,SDL_Surface *ecran);
+int Verify_Enigme(Enigme *e,int *Game,SDL_Surface *ecran);
+void Free_Enigme(Enigme *e);
+
+
+
+#endif
